6454391ed5688025b0124eca77c3b089ceca2cf7
Jakey Light <ilightforum@gmail.com>
2015-03-10 05:37:18 -0700

    Config.php
    ....

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+	$ServerName = ""; #Enter your Server Name
+	
+	#Server Configs
+		#Server 1
+			$Server1name = ""; #Name of the first server
+			$Server1ip = ""; #IP Address Used On Client
+			$Server1port = "6113"; #Port running for server1 (usually keep)
+			$Server1pop = ""; #Population displayed on client 1-6(Full)
+		#Server 2
+			$Server2name = ""; #Name of the first server
+			$Server2ip = ""; #IP Address Used On Client
+			$Server2port = "6114"; #Port running for server2 (usually keep)
+			$Server2pop = ""; #Population displayed on client 1-6(Full)
+		#Server 3
+			$Server3name = ""; #Name of the first server
+			$Server3ip = ""; #IP Address Used On Client
+			$Server3port = "6115"; #Port running for server3 (usually keep)
+			$Server3pop = ""; #Population displayed on client 1-6(Full)
+
+	#Items and name for the bot.
+		$BotName = "Bot"; #The Bots Name
+		#Bot Clothing
+			$BotColor = "4"; #Bots Color ID
+			$BotHead = "436"; #Bots Head ID
+			$BotFace = "106"; #Bots Face ID
+			$BotNeck = "179"; #Bots Neck ID
+			$BotBody = "221"; #Bots Body ID
+			$BotHand = "0"; #Bots Hand ID
+			$BotFeet = "366"; #Bots Feet ID
+			
+?>
